2e33d45c124b8635a24eb42deabe8f2dd67a1325
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2023-04-11 20:02:29 +0300

    Add utilities for writing and reading files

---
+++ app/src/main/java/calibration/image/FileUtil.java
@@ -0,0 +1,54 @@
+package calibration.image;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FileUtil {
+
+    public static final String ORIGINAL_FILE = "../data/1_xray_frame_896x1088.raw";
+    public static final String OFFSET_FILE = "../data/2_offset_data_896x1088.raw";
+    public static final String DEFECT_FILE = "../data/3_defect_map_896x1088.raw";
+    
+    private static final Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+    public static byte[] readBytes(String path, int size) throws IOException {
+
+        var buffer = new byte[size];
+
+        try (var reader = new DataInputStream(new FileInputStream(new File(path)))) {
+
+            var bytesRead = reader.read(buffer);
+
+            if (bytesRead != size) {
+                logger.warn("Read {} bytes instead of expected {}", bytesRead, size);
+            }
+
+            return buffer;
+
+        }
+
+    }
+
+    public static void writeBytes(String path, byte[] content) throws IOException {
+        var file = new File(path);
+        var filePath = file.toPath();
+        Files.createDirectories(filePath.getParent());
+        if (!file.exists()) {
+            Files.createFile(filePath);
+        }
+        try (var writer = new DataOutputStream(new FileOutputStream(file))) {
+            writer.write(content);
+            logger.info("Successfully wrote to {}", path);
+        }
+    }
+
+    private FileUtil() {}
+}
